4f75e89cf94ffe8bdae58b9054c1130eacbb51f1
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-14 18:27:15 +0200

    Update __init__.py

--- components/sd_mmc_card/__init__.py
+++ components/sd_mmc_card/__init__.py
@@ -25,7 +25,7 @@ CONF_POWER_CTRL_PIN = "power_ctrl_pin"
 sd_mmc_card_component_ns = cg.esphome_ns.namespace("sd_mmc_card")
 SdMmc = sd_mmc_card_component_ns.class_("SdMmc", cg.Component)
 
-# Action
+# Actions
 SdMmcWriteFileAction = sd_mmc_card_component_ns.class_("SdMmcWriteFileAction", automation.Action)
 SdMmcAppendFileAction = sd_mmc_card_component_ns.class_("SdMmcAppendFileAction", automation.Action)
 SdMmcCreateDirectoryAction = sd_mmc_card_component_ns.class_("SdMmcCreateDirectoryAction", automation.Action)
@@ -37,21 +37,19 @@ def validate_raw_data(value):
         return value.encode("utf-8")
     if isinstance(value, list):
         return cv.Schema([cv.hex_uint8_t])(value)
-    raise cv.Invalid(
-        "data must either be a string wrapped in quotes or a list of bytes"
-    )
+    raise cv.Invalid("data must either be a string wrapped in quotes or a list of bytes")
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(SdMmc),
         cv.Required(CONF_CLK_PIN): pins.internal_gpio_output_pin_number,
         cv.Required(CONF_CMD_PIN): pins.internal_gpio_output_pin_number,
-        cv.Required(CONF_DATA0_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
-        cv.Optional(CONF_DATA1_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
-        cv.Optional(CONF_DATA2_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
-        cv.Optional(CONF_DATA3_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
+        cv.Required(CONF_DATA0_PIN): pins.internal_gpio_input_output_pin_number,
+        cv.Optional(CONF_DATA1_PIN): pins.internal_gpio_input_output_pin_number,
+        cv.Optional(CONF_DATA2_PIN): pins.internal_gpio_input_output_pin_number,
+        cv.Optional(CONF_DATA3_PIN): pins.internal_gpio_input_output_pin_number,
         cv.Optional(CONF_MODE_1BIT, default=False): cv.boolean,
-        cv.Optional(CONF_POWER_CTRL_PIN) : pins.gpio_pin_schema({
+        cv.Optional(CONF_POWER_CTRL_PIN): pins.gpio_pin_schema({
             CONF_OUTPUT: True,
             CONF_PULLUP: False,
             CONF_PULLDOWN: False,
@@ -59,27 +57,26 @@ CONFIG_SCHEMA = cv.Schema(
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
-
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
     cg.add(var.set_mode_1bit(config[CONF_MODE_1BIT]))
-
     cg.add(var.set_clk_pin(config[CONF_CLK_PIN]))
     cg.add(var.set_cmd_pin(config[CONF_CMD_PIN]))
     cg.add(var.set_data0_pin(config[CONF_DATA0_PIN]))
 
-    if (config[CONF_MODE_1BIT] == False):
-        cg.add(var.set_data1_pin(config[CONF_DATA1_PIN]))
-        cg.add(var.set_data2_pin(config[CONF_DATA2_PIN]))
-        cg.add(var.set_data3_pin(config[CONF_DATA3_PIN]))
+    if not config[CONF_MODE_1BIT]:
+        if CONF_DATA1_PIN in config:
+            cg.add(var.set_data1_pin(config[CONF_DATA1_PIN]))
+        if CONF_DATA2_PIN in config:
+            cg.add(var.set_data2_pin(config[CONF_DATA2_PIN]))
+        if CONF_DATA3_PIN in config:
+            cg.add(var.set_data3_pin(config[CONF_DATA3_PIN]))
 
-    if (CONF_POWER_CTRL_PIN in config):
+    if CONF_POWER_CTRL_PIN in config:
         power_ctrl = await cg.gpio_pin_expression(config[CONF_POWER_CTRL_PIN])
-        cg.add(var.set_power_ctrl_pin(power_ctrl));
-
-
+        cg.add(var.set_power_ctrl_pin(power_ctrl))
 
 SD_MMC_PATH_ACTION_SCHEMA = cv.Schema(
     {
@@ -108,7 +105,6 @@ async def sd_mmc_write_file_to_code(config, action_id, template_arg, args):
     cg.add(var.set_data(data_))
     return var
 
-
 @automation.register_action(
     "sd_mmc_card.append_file", SdMmcAppendFileAction, SD_MMC_WRITE_FILE_ACTION_SCHEMA
 )
@@ -121,7 +117,6 @@ async def sd_mmc_append_file_to_code(config, action_id, template_arg, args):
     cg.add(var.set_data(data_))
     return var
 
-
 @automation.register_action(
     "sd_mmc_card.create_directory", SdMmcCreateDirectoryAction, SD_MMC_PATH_ACTION_SCHEMA
 )
@@ -131,7 +126,6 @@ async def sd_mmc_create_directory_to_code(config, action_id, template_arg, args)
     path_ = await cg.templatable(config[CONF_PATH], args, cg.std_string)
     cg.add(var.set_path(path_))
     return var
-
 
 @automation.register_action(
     "sd_mmc_card.remove_directory", SdMmcRemoveDirectoryAction, SD_MMC_PATH_ACTION_SCHEMA
@@ -143,7 +137,6 @@ async def sd_mmc_remove_directory_to_code(config, action_id, template_arg, args)
     cg.add(var.set_path(path_))
     return var
 
-
 @automation.register_action(
     "sd_mmc_card.delete_file", SdMmcDeleteFileAction, SD_MMC_PATH_ACTION_SCHEMA
 )
@@ -153,3 +146,4 @@ async def sd_mmc_delete_file_to_code(config, action_id, template_arg, args):
     path_ = await cg.templatable(config[CONF_PATH], args, cg.std_string)
     cg.add(var.set_path(path_))
     return var
+
