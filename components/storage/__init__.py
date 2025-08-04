289d72e4a4886a7935a45239072254c95962aa89
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-05 00:45:20 +0200

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -1,38 +1,34 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_PLATFORM
-from esphome.core import CORE
+from esphome.const import CONF_ID
+from ..sd_mmc import CONF_SD_MMC_ID, SdMmc, sd_mmc_ns
 
-CODEOWNERS = ["@youkorr"]
-
-# Define the base storage namespace
-storage_ns = cg.esphome_ns.namespace("storage")
-Storage = storage_ns.class_("Storage", cg.Component)
+DEPENDENCIES = ["sd_mmc"]
 
 # Configuration constants
 CONF_PATH_PREFIX = "path_prefix"
 
-def storage_schema(storage_class):
-    """Create a storage schema for a specific storage implementation"""
-    return cv.Schema({
-        cv.GenerateID(): cv.declare_id(storage_class),
+# Define the SD MMC Storage class
+SdMmcStorage = sd_mmc_ns.class_("SdMmcStorage", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(SdMmcStorage),
+        cv.Required(CONF_SD_MMC_ID): cv.use_id(SdMmc),
         cv.Optional(CONF_PATH_PREFIX, default=""): cv.string,
-    }).extend(cv.COMPONENT_SCHEMA)
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
-# Base schema for storage components
-BASE_STORAGE_SCHEMA = cv.Schema({
-    cv.Required(CONF_PLATFORM): cv.string,
-}).extend(cv.COMPONENT_SCHEMA)
-
-# This will be extended by platform-specific schemas
-CONFIG_SCHEMA = cv.All(BASE_STORAGE_SCHEMA)
-
-async def storage_to_code(config):
-    """Base storage setup code"""
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    if CONF_PATH_PREFIX in config:
+    # Get the SD MMC component reference
+    sd_mmc_component = await cg.get_variable(config[CONF_SD_MMC_ID])
+    cg.add(var.set_sd_mmc(sd_mmc_component))
+    
+    # Set path prefix if specified
+    if CONF_PATH_PREFIX in config and config[CONF_PATH_PREFIX]:
         cg.add(var.set_path_prefix(config[CONF_PATH_PREFIX]))
     
     return var
