08dbafccba5bbdac8526a4ecc2fd7540b6993208
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-05 00:03:32 +0200

    Update sensor.py

--- components/sd_mmc/sensor.py
+++ components/sd_mmc/sensor.py
@@ -9,11 +9,11 @@ from esphome.const import (
 )
 from . import (
     SdMmc,
-    CONF_SD_MMC_CARD_ID,
+    CONF_SD_MMC_ID,
     CONF_PATH,
 )
 
-DEPENDENCIES = ["sd_mmc_card"]
+DEPENDENCIES = ["sd_mmc"]
 
 CONF_USED_SPACE = "used_space"
 CONF_TOTAL_SPACE = "total_space"
@@ -30,7 +30,7 @@ BASE_CONFIG_SCHEMA = sensor.sensor_schema(
     state_class=STATE_CLASS_MEASUREMENT,
 ).extend(
     {
-        cv.GenerateID(CONF_SD_MMC_CARD_ID): cv.use_id(SdMmc),
+        cv.GenerateID(CONF_SD_MMC_ID): cv.use_id(SdMmc),
     }
 )
 
@@ -50,7 +50,7 @@ CONFIG_SCHEMA = cv.typed_schema(
 
 
 async def to_code(config):
-    sd_mmc_component = await cg.get_variable(config[CONF_SD_MMC_CARD_ID])
+    sd_mmc_component = await cg.get_variable(config[CONF_SD_MMC_ID])
     var = await sensor.new_sensor(config)
     if config[CONF_TYPE] in SIMPLE_TYPES:
         func = getattr(sd_mmc_component, f"set_{config[CONF_TYPE]}_sensor")
