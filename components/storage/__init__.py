548dc48a0785ea22e6060eed531aca421b09a718
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-05 00:41:06 +0200

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -1,35 +1,26 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-
-# Import du module officiel sans créer de boucle
-from esphome.components import storage as base_storage
-
-# Import des constantes et classes du composant sd_mmc
 from ..sd_mmc import CONF_SD_MMC_ID, SdMmc, sd_mmc_ns
 
-# Dépendances explicites
-DEPENDENCIES = ["sd_mmc", "storage"]
+DEPENDENCIES = ["sd_mmc"]
 
-# Déclaration de classe pour ce composant personnalisé
-sd_mmc_storage_ns = cg.esphome_ns.namespace("sd_storage")
-SdMmcStorage = sd_mmc_storage_ns.class_("SdMmcStorage", cg.Component)
+# Define the storage schema directly instead of importing
+CONF_STORAGE_ID = "storage_id"
 
-# Schéma de configuration
-CONFIG_SCHEMA = base_storage.storage_schema(SdMmcStorage).extend(
+sd_mmc_storage = sd_mmc_ns.class_("SD_MMC_Storage", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(SdMmcStorage),
+        cv.GenerateID(): cv.declare_id(sd_mmc_storage),
         cv.Required(CONF_SD_MMC_ID): cv.use_id(SdMmc),
     }
-)
+).extend(cv.COMPONENT_SCHEMA)
 
-# Code de génération
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
+    
     sd_mmc_component = await cg.get_variable(config[CONF_SD_MMC_ID])
     cg.add(var.set_sd_mmc(sd_mmc_component))
-
-    await base_storage.storage_to_code(config)
 
