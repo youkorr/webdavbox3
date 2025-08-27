14ae644f612e58bc224d8724591d3f594a541c83
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-27 05:26:07 +0200

    Update __init__.py

--- components/sd_mmc_card/__init__.py
+++ components/sd_mmc_card/__init__.py
@@ -64,11 +64,8 @@ CONFIG_SCHEMA = cv.Schema(
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
-if (this->power_ctrl_pin_) {
-    this->power_ctrl_pin_->pin_mode(gpio::FLAG_OUTPUT);
-    this->power_ctrl_pin_->digital_write(true); 
-    
-}
+
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
